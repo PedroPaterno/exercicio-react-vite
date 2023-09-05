e28e5630ca348cea69376f606babc3471d446949
PedroPaterno <rm99726@fiap.com.br>
2023-09-05 17:46:21 -0300

    adicionando componentes Menu e Rodape ao App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Menu from './components/Menu'
+import Rodape from './components/Rodape'
 
 function App() {
 
   return (
     <>
+      <Menu/>
       <Outlet/>
+      <Rodape/>
     </>
   )
 }
