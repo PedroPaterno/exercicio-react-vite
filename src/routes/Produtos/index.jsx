fdcee8a0c2ac390e23981d915443be407ab478ec
PedroPaterno <rm99726@fiap.com.br>
2023-09-05 18:06:06 -0300

    passando os dados da lista para   pagina produtos

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,8 +1,18 @@
+import { listaProdutos } from "../../components/listaProdutos";
+import { Link } from "react-router-dom";
+
 export default function Produtos(){
 
     return(
         <main>
             <h1>Produtos</h1>
+            {listaProdutos.map(prod => (
+                <div key={prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar o produto: {prod.nome}
+                    </Link>
+                </div>    
+            ))}
         </main>
     )
 }
